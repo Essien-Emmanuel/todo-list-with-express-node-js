9f168c99169a60d1ae888ab53ca74062a89b0937
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-08-03 23:06:23 +0100

    made some little changes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const date = require(__dirname + "/date.js");
+// const date = require(__dirname + "/date.js"); // most times you don't import with absolute path
+const date = require("./date"); // changed the import of date
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.get("/", (req, res) => {
   });
 });
 
+/* Telling express to use the css folder in the public folder. */
 app.use("/css", express.static(path.join(__dirname, "public", "css")));
 
 app.post("/", (req, res) => {
